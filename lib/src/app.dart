43a7f0631e46557a53530075a72d0b2ad4a04f7b
brunoalfred <alfredybruno@gmail.com>
2021-06-09 15:24:39 +0300

    Register the Provider at the top of widget tree

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,22 +9,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [ChangeNotifierProvider(create: (_) => AuthProvider())], child:  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Food App',
-      theme: ThemeData(
-        fontFamily: 'Inter',
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: kSecondaryColor),
-          bodyText1: TextStyle(color: kSecondaryColor),
-        ),
-      ),
-      // home: HomeScreen(),
-      home: SplashScreen(),
-      initialRoute: SplashScreen.routeName,
-      routes: routes,
-    ));
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AuthProvider()),
+          ChangeNotifierProvider(create: (_) => UserProvider())
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Food App',
+          theme: ThemeData(
+            fontFamily: 'Inter',
+            primaryColor: kPrimaryColor,
+            scaffoldBackgroundColor: Colors.white,
+            textTheme: TextTheme(
+              bodyText2: TextStyle(color: kSecondaryColor),
+              bodyText1: TextStyle(color: kSecondaryColor),
+            ),
+          ),
+          // home: HomeScreen(),
+          home: SplashScreen(),
+          initialRoute: SplashScreen.routeName,
+          routes: routes,
+        ));
   }
 }
