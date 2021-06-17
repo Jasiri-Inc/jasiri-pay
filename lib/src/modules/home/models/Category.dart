eb32386c7bb513064f0af04a927eeb11c32ec16d
brunoalfred <alfredybruno@gmail.com>
2021-06-17 14:39:58 +0300

    Implemented toggling between cafeterias

---
+++ lib/src/modules/home/models/Category.dart
@@ -0,0 +1,14 @@
+class CategoryModel {
+  List<String> _categoryName = [
+    'CHSS Cafe',
+    'COBE Cafe',
+    'CIVE Cafe',
+    'COED Cafe',
+    'CHSS Cafe',
+    'COBE Cafe',
+    'CIVE Cafe',
+    'COED Cafe',
+  ];
+
+  List<String> get categoryName => _categoryName;
+}
